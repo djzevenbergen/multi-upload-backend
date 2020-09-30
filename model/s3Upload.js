1c7b57693e323b75fe590690212c0fd7ff4d898c
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-09-30 16:54:07 -0700

    reconfig

--- model/s3Upload.js
+++ model/s3Upload.js
@@ -22,7 +22,11 @@ router.post('/upload', multipleUpload, function (req, res) {
 
   AWS.config.loadFromPath('./config.json');
 
-  let s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+  let s3 = new aws.S3({
+    apiVersion: '2006-03-01',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  });
 
   var ResponseData = [];
 
