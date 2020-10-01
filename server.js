57abb2244a44c9476d23cdaafb1382b82a4c2b65
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-09-30 17:12:57 -0700

    remove config line

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.post('/api/upload', multipleUpload, function (req, res) {
 
   AWS.config.update({ region: 'us-east-1' });
 
-  let s3 = new aws.S3({
+  let s3 = new AWS.S3({
     apiVersion: '2006-03-01',
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
